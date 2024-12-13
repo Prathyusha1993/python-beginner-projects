996e008777840a728d776ada09b4596ecd983ca0
Prathyusha Kurigala <prathyushakiran@Prathyushas-MBP.attlocal.net>
2024-12-12 16:28:59 -0800

    python beginner projects

--- currency_converter.py
+++ currency_converter.py
@@ -1,5 +1,43 @@
 
+def get_amount():
+    while True:
+        try:
+            total = float(input('Enter the amount: '))
+            if total <= 0:
+                raise ValueError()
+            return total
+        except ValueError:
+            print('Invalid amount')
 
-amount = input('Enter the amount: ')
-source = input('Source currency (USD/EUR/CAD): ')
-target = input('Target currency (USD/EUR/CAD): ')
+def get_currency(label):
+    currencies = ('USD', 'EUR', 'CAD')
+    while True:
+        currency = input(f'{label} currency (USD/EUR/CAD): ').upper()
+        if currency not in currencies:
+            print('Invalid currency')
+        else:
+            return currency
+
+def converter(amount, source_currency, target_currency):
+    exchange_rates = {
+        'USD': {'EUR': 0.85, 'CAD': 1.25},
+        'EUR': {'USD': 1.18, 'CAD': 1.47},
+        'CAD': {'EUR': 0.68, 'USD': 0.80}
+    }
+
+    if source_currency == target_currency:
+        return amount
+
+    return amount * exchange_rates[source_currency][target_currency]
+
+
+def main():
+    amount = get_amount()
+    source_currency = get_currency('Source')
+    target_currency = get_currency('Target')
+    converted_amount = converter(amount, source_currency, target_currency)
+    print(f'{amount} {source_currency} is equal to {converted_amount:.2f}')
+
+
+# if __name__ == '__main__':
+main()
