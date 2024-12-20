509891ee9dfe8663f47ef4ed649718549939e0fd
Prathyusha Kurigala <prathyushakiran@Prathyushas-MBP.attlocal.net>
2024-12-19 17:10:23 -0800

    python beginner projects

---
+++ atm.py
@@ -0,0 +1,94 @@
+# simple atm system
+# it allows to check balance, deposit money, withdraw funds
+# need to use OOP concept
+
+class ATM:
+    def __init__(self):
+        self.balance = 0
+
+    def check_balance(self):
+        return self.balance
+
+    def deposit(self, amount):
+        if amount <= 0:
+            raise ValueError('Deposit amount must be positive')
+
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if amount <= 0:
+            raise ValueError('withdrawal must be positive.')
+        if amount > self.balance:
+            raise ValueError('Insufficient funds.')
+
+        self.balance -= amount
+
+
+class ATMController:
+    def __init__(self):
+        self.atm = ATM()
+
+    def get_number(self, prompt):
+        while True:
+            try:
+                number = float(input(prompt))
+                return number
+            except ValueError:
+                print('Please enter a valid number')
+
+
+    def display_menu(self):
+        print('Welcome to ATM!')
+        print('1. Check Balance')
+        print('2. Deposit')
+        print('3. Withdraw')
+        print('4. Exit')
+
+    def check_balance(self):
+        balance = self.atm.check_balance()
+        print(f'your current balance is ${balance}')
+
+    def deposit(self):
+        while True:
+            try:
+                amount = self.get_number('Enter the amount to deposit:')
+                self.atm.deposit(amount)
+                print(f'successfully deposited ${amount}')
+                break
+            except ValueError as error:
+                print(error)
+
+    def withdraw(self):
+        while True:
+            try:
+                amount = self.get_number('Enter the amount to withdraw:')
+                self.atm.withdraw(amount)
+                print(f'successfully withdrew ${amount}')
+                break
+            except ValueError as error:
+                print(error)
+
+    def run(self):
+        while True:
+            self.display_menu()
+
+            choice = input('Please choose an option: ')
+            if choice == '1':
+                self.check_balance()
+            elif choice == '2':
+                self.deposit()
+            elif choice == '3':
+                self.withdraw()
+            elif choice == '4':
+                print('Thank you for using atm')
+                break
+            else:
+                print('Invalid choice.please try again')
+
+
+def main():
+    atm = ATMController()
+    atm.run()
+
+
+main()
